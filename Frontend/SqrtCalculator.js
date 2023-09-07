a13d33255e847ce1c09e94149b1984d30d0a82ac
Uraswa <33593469+Uraswa@users.noreply.github.com>
2023-09-07 13:57:25 +0300

    empty input bug fix

--- Frontend/SqrtCalculator.js
+++ Frontend/SqrtCalculator.js
@@ -3,10 +3,10 @@
 let curLang = 'ru';
 let debugVersion = true;
 
-function changeLanguage(language){
+function changeLanguage(language) {
     curLang = language;
     let texts = document.getElementsByClassName('ILanguage');
-    for (let text of texts){
+    for (let text of texts) {
         text.innerHTML = dictionary[language][text.getAttribute('key')];
     }
 }
@@ -19,11 +19,11 @@ const precisionInput = document.getElementById('precision_input');
 const rootExponentInput = document.getElementById('root_exponent_input');
 const resultView = document.getElementById('number_result');
 
-function renderError(error_key){
-    resultView.innerHTML = '<span class="ILanguage" key="'+error_key+'">' + dictionary[curLang][error_key] + '</span>'
+function renderError(error_key) {
+    resultView.innerHTML = '<span class="ILanguage" key="' + error_key + '">' + (dictionary[curLang][error_key] || '') + '</span>'
 }
 
-function Calculate(val){
+function Calculate(val) {
     val = val
         .replaceAll('sin', 'Math.sin')
         .replaceAll('cos', 'Math.cos')
@@ -34,21 +34,24 @@ function Calculate(val){
 
     if (val === "") {
         resultView.innerHTML = "";
-        return;
+        return {
+            success: false,
+            error: ''
+        }
     }
 
     try {
         val = Number.parseFloat(eval(val));
     } catch (e) {
 
-        if (e instanceof ReferenceError){
+        if (e instanceof ReferenceError) {
             return {
                 success: false,
                 error: 'err_unknown_symbol'
             }
         }
 
-        if (e instanceof SyntaxError){
+        if (e instanceof SyntaxError) {
             return {
                 success: false,
                 error: 'err_unknown_symbol'
@@ -60,14 +63,14 @@ function Calculate(val){
 
     let rootExponent = Number.parseFloat(rootExponentInput.value);
 
-    if (val < 0 && rootExponent % 2 === 0){
+    if (val < 0 && rootExponent % 2 === 0) {
         return {
             success: false,
             error: 'err_negative_value'
         }
     }
 
-    if (rootExponent === 0){
+    if (rootExponent === 0) {
         return {
             success: false,
             error: 'err_wrong_root_exponent_value'
@@ -83,14 +86,14 @@ function Calculate(val){
 
     let precisionVal = precisionInput.value;
 
-    if (precisionVal < 0 || precisionVal > 100){
+    if (precisionVal < 0 || precisionVal > 100) {
         return {
             success: false,
             error: 'err_too_large_precision'
         }
     }
 
-    if (precisionVal % 1 !== 0){
+    if (precisionVal % 1 !== 0) {
         return {
             success: false,
             error: 'err_floating_precision'
@@ -100,7 +103,7 @@ function Calculate(val){
     // костыль чтобы без муавра. Например, кубический корень из -8
 
     let sqVal = 0;
-    if (val > 0){
+    if (val > 0) {
         sqVal = Math.pow(val, 1 / rootExponent);
     } else {
         sqVal = -Math.pow(Math.abs(val), 1 / rootExponent);
@@ -108,7 +111,7 @@ function Calculate(val){
 
     // вообще до сюда не должно дойти, но если вдруг что-то еще не учли здесь выведется
 
-    if (Number.isNaN(sqVal)){
+    if (Number.isNaN(sqVal)) {
         return {
             success: false,
             error: 'err_nan'
@@ -122,11 +125,11 @@ function Calculate(val){
     }
 }
 
-function workCalculation(){
+function workCalculation() {
     let expression = numberInput.value;
     let result = Calculate(expression);
 
-    if (result.success){
+    if (result.success) {
         resultView.innerHTML = result.value;
         return;
     }
@@ -138,28 +141,28 @@ numberInput.addEventListener('input', workCalculation);
 precisionInput.addEventListener('input', workCalculation)
 rootExponentInput.addEventListener('input', workCalculation)
 
-function cot(val){
+function cot(val) {
     return Math.pow(Math.tan(val), -1);
 }
 
 //input
 
-function workInput(character){
+function workInput(character) {
 
     let cachedCursorIndex = numberInput.selectionStart;
     let beforeString = numberInput.value.slice(0, cachedCursorIndex);
 
-    if (character === '<-'){
+    if (character === '<-') {
         if (cachedCursorIndex !== 0) {
             numberInput.value = beforeString.slice(0, cachedCursorIndex - 1) + numberInput.value.substring(cachedCursorIndex, numberInput.value.length);
         }
         numberInput.focus();
         numberInput.selectionStart = cachedCursorIndex - 1;
 
-    } else if (character === '+-'){
+    } else if (character === '+-') {
         let currentCharacter = numberInput.value.slice(cachedCursorIndex, cachedCursorIndex + 1);
 
-        if (currentCharacter !== "-"){
+        if (currentCharacter !== "-") {
             numberInput.value = beforeString + '-' + numberInput.value.substring(cachedCursorIndex, numberInput.value.length);
         } else {
             numberInput.value = beforeString + numberInput.value.substring(cachedCursorIndex + 1, numberInput.value.length);
@@ -168,21 +171,19 @@ function workInput(character){
         numberInput.focus();
         numberInput.selectionStart = cachedCursorIndex;
 
-    } else if (character === 'C'){
+    } else if (character === 'C') {
         numberInput.value = "";
         numberInput.focus();
         numberInput.selectionStart = 0;
-    }
-    else if (character === '='){
+    } else if (character === '=') {
         numberInput.focus();
-        numberInput.selectionStart = cachedCursorIndex  ;
-    }
-    else if (character === 'cos' || character === 'sin' || character === 'ctg' || character === 'sqrt' || character === 'tg'){
+        numberInput.selectionStart = cachedCursorIndex;
+    } else if (character === 'cos' || character === 'sin' || character === 'ctg' || character === 'sqrt' || character === 'tg') {
         numberInput.value = beforeString + character + '()' + numberInput.value.substring(cachedCursorIndex, numberInput.value.length);
         numberInput.focus();
         numberInput.selectionStart = cachedCursorIndex + character.length + 1;
     } else {
-        numberInput.value = beforeString + character +  numberInput.value.substring(cachedCursorIndex, numberInput.value.length);
+        numberInput.value = beforeString + character + numberInput.value.substring(cachedCursorIndex, numberInput.value.length);
         numberInput.focus();
         numberInput.selectionStart = cachedCursorIndex + character.length;
     }
@@ -203,6 +204,6 @@ changeLanguage(curLang);
 
 // service
 
-function print(...args){
+function print(...args) {
     if (debugVersion) console.log(args.join(" "))
 }
