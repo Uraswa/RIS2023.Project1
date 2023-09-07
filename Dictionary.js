7df7c60cea8e6d3f8e96bd20b5123e86ce08c1dd
Uraswa <33593469+Uraswa@users.noreply.github.com>
2023-09-07 13:48:12 +0300

    init

---
+++ Dictionary.js
@@ -0,0 +1,47 @@
+const dictionary = {
+    'ru': {
+        'err_negative_value': 'Ошибка: отрицательное число под корнем четной степени!',
+        'enter_precision': 'Количество знаков после запятой в результате',
+        'enter_number': 'Выражение',
+        'text_result': 'Результат:',
+        'text_language' : 'Язык: ',
+        'enter_sqrt_extent':'Показатель корня',
+        'err_wrong_root_exponent_value': 'Неправильный показатель корня',
+        'err_negative_value_float_root_exponent': 'Отрицательное число под корнем дробной степени',
+        'err_unknown_symbol': 'Ошбика: Неизвестный символ',
+        'err_nan': 'Ошибка: Проверьте корректность введенного выражения',
+        'err_too_large_precision': 'Ошибка: Максимальное количество знаков после запятой = 100',
+        'err_floating_precision' : 'Ошибка: Количество знаков после запятой - это целое число',
+        'text_developed': 'Разработано'
+    },
+    'en': {
+        'err_negative_value': 'Error: wrong sqrt argument (must be non-negative)',
+        'enter_precision': 'Float number precision',
+        'enter_number': 'Enter number',
+        'text_result': 'Result:',
+        'text_language' : 'Language: ',
+        'enter_sqrt_extent': 'Root exponent',
+        'err_wrong_root_exponent_value': 'Wrong root exponent value',
+        'err_negative_value_float_root_exponent': 'Negative value under the root with floating exponent'
+    },
+    'ch': {
+        'err_negative_value': '错误：偶数根下为负数!',
+        'enter_precision': '浮点数精度',
+        'enter_number': '输入数字',
+        'text_result': '结果:',
+        'text_language' : '语言: ',
+        'enter_sqrt_extent': '根索引',
+        'err_wrong_root_exponent_value': '不正确的根索引',
+        'err_negative_value_float_root_exponent': '分数级根下的负数'
+    },
+    'sp': {
+        'err_negative_value': 'Error: ¡número negativo bajo la raíz de grado par!',
+        'enter_precision': 'Precisión del número flotante',
+        'enter_number': 'Escriba un número',
+        'text_result': 'Resultado:',
+        'text_language' : 'Idioma: ',
+        'enter_sqrt_extent': 'Índice de raíces',
+        'err_wrong_root_exponent_value': 'Exponente de la raíz incorrecto',
+        'err_negative_value_float_root_exponent': 'Número negativo bajo la raíz de un grado fraccionario'
+    }
+};
